21607afd65b228efe9da9d9b02d6ef5ac46db31b
Christopher Rydahl <chris@medgo.fr>
2022-12-09 18:44:24 +0100

    fix: add a new action to sync postman with swagger

--- src/main.ts
+++ src/main.ts
@@ -6,103 +6,104 @@ async function run(): Promise<string> {
     const postmanApiKey: string = core.getInput('postman-api-key')
     const workspaceId: string = core.getInput('workspace-id')
     //const collectionName: string = core.getInput('collection-name')
-    const input: string = core.getInput('openapi-json')
-
+    //const input: string = core.getInput('openapi-json')
+    let input = `{
+      "openapi": "3.0.0",
+      "info": {
+          "version": "1.0.0",
+          "title": "Test API"
+      },
+      "servers": [
+          {
+              "url": "http://locahost:3000"
+          }
+      ],
+      "paths": {
+          "/user": {
+              "get": {
+                  "summary": "List all users",
+                  "operationId": "listUser",
+                  "parameters": [
+                      {
+                          "name": "id",
+                          "in": "query",
+                          "required": true,
+                          "description": "The user's ID.",
+                          "example": 1234,
+                          "schema": {
+                              "type": "integer",
+                              "format": "int32"
+                          }
+                      }
+                  ],
+                  "responses": {
+                      "200": {
+                          "description": "Information about the user.",
+                          "headers": {
+                              "x-next": {
+                                  "description": "A link to the next page of responses.",
+                                  "schema": {
+                                      "type": "string"
+                                  }
+                              }
+                          },
+                          "content": {
+                              "application/json": {
+                                  "schema": {
+                                      "$ref": "#/components/schemas/User"
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      },
+      "components": {
+          "schemas": {
+              "User": {
+                  "type": "object",
+                  "required": [
+                      "id",
+                      "name"
+                  ],
+                  "properties": {
+                      "id": {
+                          "type": "integer",
+                          "format": "int64"
+                      },
+                      "name": {
+                          "type": "string"
+                      },
+                      "tag": {
+                          "type": "string"
+                      }
+                  }
+              },
+              "Error": {
+                  "type": "object",
+                  "required": [
+                      "code",
+                      "message"
+                  ],
+                  "properties": {
+                      "code": {
+                          "type": "integer",
+                          "format": "int32"
+                      },
+                      "message": {
+                          "type": "string"
+                      }
+                  }
+              }
+          }
+      }
+  }`
+    input = JSON.parse(input)
     const data = {
       workspace: '6ca7e119-fce5-456e-86cd-f6c623c19ff4',
       type: 'json',
-      input: `{
-        "openapi": "3.0.0",
-        "info": {
-            "version": "1.0.0",
-            "title": "Test API"
-        },
-        "servers": [
-            {
-                "url": "http://locahost:3000"
-            }
-        ],
-        "paths": {
-            "/user": {
-                "get": {
-                    "summary": "List all users",
-                    "operationId": "listUser",
-                    "parameters": [
-                        {
-                            "name": "id",
-                            "in": "query",
-                            "required": true,
-                            "description": "The user's ID.",
-                            "example": 1234,
-                            "schema": {
-                                "type": "integer",
-                                "format": "int32"
-                            }
-                        }
-                    ],
-                    "responses": {
-                        "200": {
-                            "description": "Information about the user.",
-                            "headers": {
-                                "x-next": {
-                                    "description": "A link to the next page of responses.",
-                                    "schema": {
-                                        "type": "string"
-                                    }
-                                }
-                            },
-                            "content": {
-                                "application/json": {
-                                    "schema": {
-                                        "$ref": "#/components/schemas/User"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        "components": {
-            "schemas": {
-                "User": {
-                    "type": "object",
-                    "required": [
-                        "id",
-                        "name"
-                    ],
-                    "properties": {
-                        "id": {
-                            "type": "integer",
-                            "format": "int64"
-                        },
-                        "name": {
-                            "type": "string"
-                        },
-                        "tag": {
-                            "type": "string"
-                        }
-                    }
-                },
-                "Error": {
-                    "type": "object",
-                    "required": [
-                        "code",
-                        "message"
-                    ],
-                    "properties": {
-                        "code": {
-                            "type": "integer",
-                            "format": "int32"
-                        },
-                        "message": {
-                            "type": "string"
-                        }
-                    }
-                }
-            }
-        }
-    }`
+      input
     }
 
     await axios.post('https://api.getpostman.com/import/openapi', data, {
